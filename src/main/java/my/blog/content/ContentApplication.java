15ae461ec42ab7d0d51a1618143f73881618a72a
bineea <33547731+bineea@users.noreply.github.com>
2020-03-20 00:25:28 +0800

    对称加密配置信息

--- src/main/java/my/blog/content/ContentApplication.java
+++ src/main/java/my/blog/content/ContentApplication.java
@@ -2,8 +2,12 @@ package my.blog.content;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
 /*@SpringBootApplication 告诉Spring Boot框架，这是项目的引导类。实现服务的核心初始化逻辑*/
 @SpringBootApplication
+//允许访问/refresh端点，强制Spring Boot应用重新读取程序配置
+@RefreshScope
 public class ContentApplication {
 
     public static void main(String[] args) {
