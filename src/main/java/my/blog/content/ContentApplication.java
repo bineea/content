bbe6f060323d8d31f1cfc25cd2e16be255e6c2cd
bineea <33547731+bineea@users.noreply.github.com>
2020-03-24 23:18:12 +0800

    初步配置eruaka

--- src/main/java/my/blog/content/ContentApplication.java
+++ src/main/java/my/blog/content/ContentApplication.java
@@ -3,11 +3,13 @@ package my.blog.content;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /*@SpringBootApplication 告诉Spring Boot框架，这是项目的引导类。实现服务的核心初始化逻辑*/
 @SpringBootApplication
 //允许访问/refresh端点，强制Spring Boot应用重新读取程序配置
 @RefreshScope
+@EnableEurekaClient
 public class ContentApplication {
 
     public static void main(String[] args) {
