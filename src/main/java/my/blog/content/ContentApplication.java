54bcf4257660b4f7fb0ca7de10aeb6c3ba51ddda
bineea <33547731+bineea@users.noreply.github.com>
2020-03-25 23:20:31 +0800

    配置feign客户端

--- src/main/java/my/blog/content/ContentApplication.java
+++ src/main/java/my/blog/content/ContentApplication.java
@@ -4,12 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-/*@SpringBootApplication 告诉Spring Boot框架，这是项目的引导类。实现服务的核心初始化逻辑*/
+//@SpringBootApplication 告诉Spring Boot框架，这是项目的引导类。实现服务的核心初始化逻辑
 @SpringBootApplication
+
 //允许访问/refresh端点，强制Spring Boot应用重新读取程序配置
 @RefreshScope
-@EnableEurekaClient
+//@EnableEurekaClient
+
+//启用Feign客户端
+@EnableFeignClients
 public class ContentApplication {
 
     public static void main(String[] args) {
