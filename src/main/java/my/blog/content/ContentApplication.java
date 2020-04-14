f86bb01c785746a9e6fc6e39e7f03f1ecd716c29
bineea <33547731+bineea@users.noreply.github.com>
2020-04-14 23:11:32 +0800

    配置hystrix

--- src/main/java/my/blog/content/ContentApplication.java
+++ src/main/java/my/blog/content/ContentApplication.java
@@ -2,8 +2,8 @@ package my.blog.content;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 //@SpringBootApplication 告诉Spring Boot框架，这是项目的引导类。实现服务的核心初始化逻辑
@@ -11,10 +11,12 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 //允许访问/refresh端点，强制Spring Boot应用重新读取程序配置
 @RefreshScope
-//@EnableEurekaClient
 
 //启用Feign客户端
 @EnableFeignClients(basePackages = {"my.blog.account.api"})
+
+//启用Hystrix
+@EnableCircuitBreaker
 public class ContentApplication {
 
     public static void main(String[] args) {
